9e7d34a7760d0c4c4882242f18963a4229bc6247
diegocolonia <diegocolonia98@yahoo.com>
2023-02-26 01:21:14 -0500

    commit

---
+++ src/setupService.ts
@@ -0,0 +1,11 @@
+import {Application, json, urlencoded, Response, Request, NextFunction} from 'express';
+
+//sudo npm i --save @types/express install for the imports to work 
+export class chattyService{
+
+    private app: Application;
+
+
+
+
+}
