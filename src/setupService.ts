2636ddd9b50bc7c1f5dce9c5070670e48eb63e13
diegocolonia <diegocolonia98@yahoo.com>
2023-02-26 01:37:34 -0500

    commit

--- src/setupService.ts
+++ src/setupService.ts
@@ -1,11 +1,37 @@
 import {Application, json, urlencoded, Response, Request, NextFunction} from 'express';
-
+import http from 'http'
 //sudo npm i --save @types/express install for the imports to work 
 export class chattyService{
 
     private app: Application;
 
+    constructor(app:Application){
+        this.app =app;
+    }
 
+
+    public stats():void{
+        this.secutiryMiddleware(this.app);
+        this.standardMiddleware(this.app);
+        this.routeMiddleware(this.app);
+        this.globalErrorHandler(this.app);
+        this.startServer(this.app)
+
+    }
+
+    private secutiryMiddleware(app:Application):void{}
+
+    private standardMiddleware(app:Application):void{}
+
+    private routeMiddleware(app:Application):void{}
+
+    private globalErrorHandler(app:Application):void{}
+
+    private startServer(app:Application):void{}
+
+    private createSocketID(httpServer:http.Server):void{}
+
+    private startHttpServer(httpServer:http.Server): void{}
 
 
 }
