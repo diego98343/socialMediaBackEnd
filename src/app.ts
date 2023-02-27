c50a73d20a372e4b0e295c5ab3c816296ac95686
diegocolonia <diegocolonia98@yahoo.com>
2023-02-27 02:08:46 -0500

    commit

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import express, { Express } from "express";
 import { chattyServer } from "./setupService";
 
 class Application{
+
     public initialize(): void{
         const app:Express =express();
-        
+        const server:chattyServer =new chattyServer(app);
+        server.stats();
     }
 }
+
